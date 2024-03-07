bde1d0368d483093f36bcb74d90f1e0b8b6f74aa
amrmahmoudabdelghany <amrkoky19@gmail.com>
2024-03-07 05:50:38 +0200

    init routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom' ; 
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <div className="App">
          
-         Hello World!!!
+         <BrowserRouter> 
+         
+            <Routes> 
+              <Route path = "/" element={<Layout/>}> 
+                <Route index element = {<Home/>} />
+                <Route path="cart" element = {<Cart/>} />
+                <Route path="*" element={<PageNotFound/>} />
+              </Route>
+            </Routes>
+
+         </BrowserRouter>
 
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react' 
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <h2>    
-      Nav
-    </h2>
+    <nav> 
+        <ul> 
+             <li> 
+                <Link to = "/"> 
+                  Home
+                </Link>
+             </li>
+             <li> 
+                <Link to = "/cart"> 
+                  Cart
+                </Link>
+             </li>
+
+        </ul>
+    </nav>
   )
 }
 
